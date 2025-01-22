42760f05caf73f8c2fa1e325a4e34aebd1d2df98
Zombfyd <Zombfyd.eth@gmail.com>
2025-01-21 17:44:38 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@mysten/sui.js': '@mysten/sui.js/dist/index.mjs',
-    }
-  },
-  optimizeDeps: {
-    include: ['@mysten/sui.js']
-  },
-  build: {
-    commonjsOptions: {
-      include: [/node_modules/],
-      transformMixedEsModules: true
-    }
-  }
+  plugins: [react()]
 });
