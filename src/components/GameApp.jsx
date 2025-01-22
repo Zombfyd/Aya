8b7a92d0947e6a765b103e0f901a6d962b8f71dd
Zombfyd <Zombfyd.eth@gmail.com>
2025-01-21 17:57:22 -0800

    Update GameApp.jsx

--- src/components/GameApp.jsx
+++ src/components/GameApp.jsx
@@ -10,6 +10,7 @@ import {
   formatSUI,
   useSuiClient
 } from '@suiet/wallet-kit';
+import { TransactionBlock } from '@mysten/sui.js/transactions';
 import '@suiet/wallet-kit/style.css';
 import './App.css';
 import config from '../config/config';
@@ -29,19 +30,18 @@ const GameApp = () => {
     try {
       console.log('Starting test transfer...');
       
-      const tx = {
-        type: 'transferSui',
-        data: {
-          recipient: '0x2d81a1b3f1e5b06e7b07b9b2f1f2b367f477f5f6e6f0e8c7d8c6f4e3d2c1b0a9',
-          amount: 1000000,
-        }
-      };
+      // Create new transaction block
+      const txb = new TransactionBlock();
+      
+      // Split coin and transfer
+      const [coin] = txb.splitCoins(txb.gas, [txb.pure(1000000)]); // 0.001 SUI
+      txb.transferObjects([coin], txb.pure('0x2d81a1b3f1e5b06e7b07b9b2f1f2b367f477f5f6e6f0e8c7d8c6f4e3d2c1b0a9'));
 
-      console.log('Transaction payload:', tx);
-      console.log('Wallet status:', wallet.status);
-      console.log('Chain:', wallet.chain);
+      console.log('Transaction block created:', txb);
 
-      const response = await wallet.signAndExecuteTransaction(tx);
+      const response = await wallet.signAndExecuteTransaction({
+        transaction: txb,
+      });
 
       console.log('Transfer response:', response);
       setDigest(response.digest);
