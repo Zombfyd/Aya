10e465a81b24028b1e73b59e41023dda2f38984a
Zombfyd <Zombfyd.eth@gmail.com>
2025-01-26 10:28:43 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,18 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
  plugins: [react()],
  server: {
-   cors: {
-     origin: "https://www.ayaonsui.xyz",
-     methods: ['GET', 'POST'],
-     credentials: true
+   cors: true,
+   headers: {
+     'Access-Control-Allow-Origin': '*',
+     'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+     'Access-Control-Allow-Headers': 'Content-Type'
+   }
+ },
+ build: {
+   rollupOptions: {
+     output: {
+       manualChunks: undefined
+     }
    }
  }
 });
