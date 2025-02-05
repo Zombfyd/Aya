223208417ec8202c761951b110c894b599f50d7c
Zombfyd <Zombfyd.eth@gmail.com>
2025-02-04 17:12:06 -0800

    Update GameApp.jsx

--- src/components/GameApp.jsx
+++ src/components/GameApp.jsx
@@ -793,7 +793,7 @@ useEffect(() => {
 useEffect(() => {
     const handleScroll = () => {
       const header = document.querySelector('header');
-      if (window.scrollY > window.innerHeight * 0.5) { // Adjust this value as needed
+      if (window.scrollY > window.innerHeight * 0.3) { // Adjust this value as needed
         header.classList.add('scrolled');
       } else {
         header.classList.remove('scrolled');
